c9765ffd3e925976572b58d568b8cf7436a31108
ChocoRamaBG <127840485+ChocoRamaBG@users.noreply.github.com>
2025-10-22 10:34:53 +0300

    Update setup_chrome.sh RC

--- setup_chrome.sh
+++ setup_chrome.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
-# create chrome folder
+# Create a folder for Chrome + driver
 mkdir -p /tmp/chrome
 cd /tmp/chrome
 
-# lightweight version of Chrome for testing
+# Download Chrome
 wget -O chrome-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/128.0.6613.84/linux64/chrome-linux64.zip
 unzip chrome-linux64.zip
 
+# Download ChromeDriver
 wget -O chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/128.0.6613.84/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 
-# export vars
+# Export env vars
 export GOOGLE_CHROME_BIN="/tmp/chrome/chrome-linux64/chrome"
 export CHROMEDRIVER_PATH="/tmp/chrome/chromedriver-linux64/chromedriver"
 
-# lighter Python deps
+# Go back to repo root before installing deps
+cd /opt/render/project/src
+
+# Install python deps
 pip install --upgrade pip
-pip install --no-cache-dir fastapi uvicorn selenium beautifulsoup4
+pip install -r backend/requirements.txt
